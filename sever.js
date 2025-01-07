4f283b030e2a2e5209752ec8c2e6a2f4280e41c5
bohao003 <mo20031024@gmail.com>
2025-01-07 23:58:33 +0800

    250107

--- sever.js
+++ sever.js
@@ -16,25 +16,45 @@ server.use(fileUpload({defCharset:'utf8', defParamCharset:'utf8'}));
 var DB = require("nedb-promises");
 var ProfolioDB = DB.create(__dirname+"/profolio.db");
 var ContactDB = DB.create(__dirname+"/contact.db");
- 
+var ServicesDB = DB.create(__dirname+"/services.db");
 
 /*ProfolioDB.insert([
 { id: "1", role: "焊接服務", photo: "img/IMG_2552.JPG" ,text:"提供高精度的焊接解決方案，適用於各類項目。"},
 { id: "2", role: "自動化技術", photo: "img/IMG_1721.JPG" ,text:"引入先進的機械手臂技術，提高生產效率。" },
 { id: "3", role: "訂製設計", photo: "img/IMG_1721.JPG" ,text:"根據客戶需求量身訂製解決方案，滿足特殊要求。"},
 ])*/
+/*ServicesDB.insert([
+    {
+        title:"焊接服務", image: "img/IMG_2552.JPG", description: "提供高精度的焊接解決方案，適用於各類項目。"
+    },
+    {   
+        title:"自動化技術", image: "img/IMG_1721.JPG", description:"引入先進的機械手臂技術，提高生產效率。"
+    },
+    {
+        title:"訂製設計", image:"img/IMG_4490.JPG", description:"根據客戶需求量身訂製解決方案，滿足特殊要求。"
+    }
+]);*/
 
 server.get("/services", (req, res)=>{
     //DB find
-    var Services=[
+    /*var Services=[
         {icon: "fa-shopping-cart", heading:"E-Commerce", text:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit."},
         {icon: "fa-laptop", heading:"Responsive Design", text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit."}
     ];
-    res.send(Services);
+    res.send(Services);*/
+    ServicesDB.find({}).then(results=>{
+        if(results != null){
+            res.send(results);
+        }else{
+            res.send("Errpr!");
+        }
+    })
+
+    
 });
 
 server.get("/profolio", (req,res)=>{
-      DB
+      
       ProfolioDB.find({}).then(results=>{
         if(results != null){
              res.send(results);
@@ -42,7 +62,7 @@ server.get("/profolio", (req,res)=>{
             res.send("Error!");
         }
       })
-})
+});
 
 server.post("/contact", async (req, res) => {
     try {
